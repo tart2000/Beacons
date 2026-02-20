9a69a33979e957dcffa2290e9548a2f8da495da7
Arthur <tart2000design@gmail.com>
2026-02-20 11:30:09 +0100

    now with cover image and a few other changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 // En prod (ex. GitHub Pages), définir PUBLISH_BASE_PATH (ex. '/Beacons/'). Plus tard, ta propre URL (ex. '/').
 export default defineConfig({
+  site: process.env.SITE || 'https://design.pocstudio.fr',
   base: process.env.PUBLISH_BASE_PATH || './',
   integrations: [tailwind()]
 });
